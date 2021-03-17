07304e63b2485f5b1cf4f200ae11359edb595b20
Dami-art <damjana.zoric@gmail.com>
2021-03-17 07:45:52 +0100

    new

---
+++ ex03/src/index.js
@@ -0,0 +1,12 @@
+Const MyEqualFunction = require("./../ex03/src");
+//Only chagne code below this line
+function myFunction(myParameter) {
+if (my Parameter) { 
+return „The Parameter is true!“;
+} else {
+return „The Parameter is false!“;
+
+Console.log(myFunction(true));
+Console.log(myFunction(false));
+//change  code under this line
+moddule.exports = myFunction;
